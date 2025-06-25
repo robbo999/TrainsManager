2ffe18d058a96c0f61fe6b4551e4ef1e6599758e
robbo999 <118767105+robbo999@users.noreply.github.com>
2025-06-25 21:39:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@supabase/supabase-js'] // ✅ Force Vite to pre-bundle this
+  }
+});
