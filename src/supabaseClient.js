21faea7b9202a2ef301ebe65e59a249c5479cd78
robbo999 <118767105+robbo999@users.noreply.github.com>
2025-07-14 20:36:37 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fuhoybxcckegcdarsyei.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1aG95YnhjY2tlZ2NkYXJzeWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4ODExMjUsImV4cCI6MjA2NjQ1NzEyNX0.X6MptprRmTGYgMwnigmtueBPRopMmB2vHwiYqGk_R7E';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
